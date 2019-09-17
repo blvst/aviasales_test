39c1e31a192669f41bd599daa8f5ba21c8f9b8af
zhubartovich.s <zhubartovich.s@wildberries.ru>
2019-09-17 21:27:49 +0300

    Search actions refactoring

--- src/actions/search.js
+++ src/actions/search.js
@@ -6,26 +6,57 @@ import {
   UPDATE_TICKETS,
 } from "../constants/actions";
 
-export const initSearch = () => async dispatch => {
-  try {
-    const response = await searchAPI.getSearchID();
 
-    return dispatch({
-      type: CHANGE_SEARCH_ID,
-      payload: response,
-    });
-  } catch (e) {
-    return dispatch({
-      type: SET_SEARCH_FAILED,
-    });
+function changeSearchId(payload) {
+  return {
+    type: CHANGE_SEARCH_ID,
+    payload,
   }
+}
+
+function changeFailedAttempts(payload) {
+  return {
+    type: CHANGE_FAILED_ATTEMPTS,
+    payload,
+  }
+}
+
+function setSearchFailed() {
+  return {
+    type: SET_SEARCH_FAILED,
+  }
+}
+
+function updateTickets(tickets) {
+  const segments = [];
+  tickets.forEach((ticket, index) => {
+    segments[index] = ticket.segments
+      .map((segment) => (segment.stops.length));
+  });
+
+  return {
+    type: UPDATE_TICKETS,
+    payload: {
+      tickets,
+      segments,
+    },
+  }
+}
+
+export const initSearch = () => dispatch => {
+  return searchAPI.getSearchID()
+    .then(response => dispatch(changeSearchId(response)))
+    .catch(() => dispatch(setSearchFailed()));
 };
 
 export const getTickets = (searchId) => async (dispatch, getState) => {
-  let result;
-  let response;
   const { failedAttempts } = getState();
 
+  if (failedAttempts >= 5) {
+    return dispatch(setSearchFailed());
+  }
+
+  let response;
   try {
     response = await searchAPI.getTickets(searchId);
 
@@ -33,40 +64,14 @@ export const getTickets = (searchId) => async (dispatch, getState) => {
       throw new Error('Ошибка сервера');
     }
 
-    const segments = [];
-    response.tickets.forEach((ticket, index) => {
-      ticket.segments.forEach((segment) => {
-        segments[index] = segments[index] || [];
-        segments[index].push(segment.stops.length);
-      });
-    });
-
-    result = {
-      type: UPDATE_TICKETS,
-      payload: {
-        tickets: response.tickets,
-        segments,
-      },
-    }
-
-  } catch (e) {
-    if (failedAttempts >= 5) {
-      result = {
-        type: SET_SEARCH_FAILED,
-      }
-    } else {
-      result = {
-        type: CHANGE_FAILED_ATTEMPTS,
-        payload: failedAttempts + 1,
-      }
-    }
+    return dispatch(updateTickets(response.tickets))
+  } catch(e) {
+    return dispatch(changeFailedAttempts(failedAttempts + 1))
   } finally {
-    if ((!response && failedAttempts < 5) || response.stop !== true) {
+    if (!response || !response.stop) {
       setTimeout(() => {
         getTickets(searchId)(dispatch, getState);
       }, 3000);
     }
   }
-
-  return dispatch(result);
 };
